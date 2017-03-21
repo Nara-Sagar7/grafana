2ff9bfa7cc3d3b5649db15c55b62ee69a8849282
shevyf <shevaunfrazier@googlemail.com>
2017-03-21 22:48:09 +0000

    linting and removing unnecessary code

--- public/app/plugins/datasource/graphite/dataviews.js
+++ public/app/plugins/datasource/graphite/dataviews.js
@@ -1,17 +1,16 @@
-var dataviews = {
+dataviews = {
   avg: {name: 'average', firstDisplayed: 'avg'},
   sum: {name: 'sum', firstDisplayed: 'sum'},
   sumX: {name: 'sum per X seconds', variable: 60, firstDisplayed: 'sum'},
   min: {name: 'minimum', firstDisplayed: 'min'},
-  min: {name: 'maximum', firstDisplayed: 'max'},
+  max: {name: 'maximum', firstDisplayed: 'max'},
   obvs: {name: 'observations', firstDisplayed: 'obvs'},
   sumrate: {name: 'sum per second', firstDisplayed: 'sumrate'},
   obvsrate: {name: 'observations per second', firstDisplayed: 'obvsrate'},
-  pctX: {name: 'percentile', variable: 90, lastDisplayed: 'pct'} 
+  pctX: {name: 'percentile', variable: 90, lastDisplayed: 'pct'}
 };
 
-var viewRender = function(view) {
+function viewRender(view) {
+  'use strict';
   return (view.firstDisplayed || '') + (view.variable || '') + (view.lastDisplayed || '')
-}; 
-
-export { dataviews, viewRender };
+};

28efd2655f8714f844b79da373e5960eb8fafff3
shevyf <shevaunfrazier@googlemail.com>
2017-03-21 23:18:22 +0000

    trying to deal with import/export issues

--- public/app/plugins/datasource/graphite/dataviews.js
+++ public/app/plugins/datasource/graphite/dataviews.js
@@ -1,16 +1,20 @@
-dataviews = {
-  avg: {name: 'average', firstDisplayed: 'avg'},
-  sum: {name: 'sum', firstDisplayed: 'sum'},
-  sumX: {name: 'sum per X seconds', variable: 60, firstDisplayed: 'sum'},
-  min: {name: 'minimum', firstDisplayed: 'min'},
-  max: {name: 'maximum', firstDisplayed: 'max'},
-  obvs: {name: 'observations', firstDisplayed: 'obvs'},
-  sumrate: {name: 'sum per second', firstDisplayed: 'sumrate'},
-  obvsrate: {name: 'observations per second', firstDisplayed: 'obvsrate'},
-  pctX: {name: 'percentile', variable: 90, lastDisplayed: 'pct'}
-};
+define({
 
-function viewRender(view) {
-  'use strict';
-  return (view.firstDisplayed || '') + (view.variable || '') + (view.lastDisplayed || '')
-};
+  var allViews = {
+    avg: {name: 'average', firstDisplayed: 'avg'},
+    sum: {name: 'sum', firstDisplayed: 'sum'},
+    sumX: {name: 'sum per X seconds', variable: 60, firstDisplayed: 'sum'},
+    min: {name: 'minimum', firstDisplayed: 'min'},
+    max: {name: 'maximum', firstDisplayed: 'max'},
+    obvs: {name: 'observations', firstDisplayed: 'obvs'},
+    sumrate: {name: 'sum per second', firstDisplayed: 'sumrate'},
+    obvsrate: {name: 'observations per second', firstDisplayed: 'obvsrate'},
+    pctX: {name: 'percentile', variable: 90, lastDisplayed: 'pct'}
+  };
+
+  function viewRender(view) {
+    'use strict';
+    return (view.firstDisplayed || '') + (view.variable || '') + (view.lastDisplayed || '')
+  };,
+
+});
